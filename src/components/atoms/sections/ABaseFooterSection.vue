570ce464a3aea865ce17a6ea56c2de8dd9e6cc4f
nekagit <nenad99@hotmail.de>
2024-05-06 13:45:42 +0200

    init

--- src/components/atoms/sections/ABaseFooterSection.vue
+++ src/components/atoms/sections/ABaseFooterSection.vue
@@ -1,14 +1,12 @@
 <template>
   
+  <AMenuButton class="h-[30vh]" />
   <footer class="bg-gray-800 text-white">
     <div class="container mx-auto py-8 px-4">
       <div class="grid grid-cols-1 md:grid-cols-2">
-        <AMenuButton class="h-[30vh]" />
-        <!-- Logo -->
         <div class="footer-logo flex items-center">
           <h1 class="text-2xl font-bold">Stichting SOP Rotterdam</h1>
         </div>
-        <!-- Social Icons -->
       </div>
       <!-- Bottom Text -->
       <div class="footer-bottom mt-8 text-center">
