abe2dff6e56fc668dd357e4f9f7f19e2b97474ff
nekagit <nenad99@hotmail.de>
2024-05-31 11:38:59 +0200

    w

--- src/components/atoms/cards/AInternetContactCard.vue
+++ src/components/atoms/cards/AInternetContactCard.vue
@@ -1,7 +1,7 @@
 
 <template>
       <div
-        class="grid sm:grid-cols-2 items-center gap-16 p-8 m-4 max-w-4xl bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md text-[#333] font-[sans-serif] overflow-x-auto"
+        class="h-screen overflow-auto grid sm:grid-cols-2 items-center gap-8 p-8 m-4 max-w-4xl bg-white shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md text-[#333] font-[sans-serif] "
       >
         <div>
           <h1 class="text-3xl font-extrabold">Let's Talk</h1>
